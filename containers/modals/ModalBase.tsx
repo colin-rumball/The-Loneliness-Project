c052806734f940e0fbfb892549f631fbafe8e094
Colin Rumball <colin.rumball@gmail.com>
2020-02-06 01:01:43 -0500

    Added gradient to modal base

--- containers/modals/ModalBase.tsx
+++ containers/modals/ModalBase.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useContext } from "react";
+import React, { useMemo } from "react";
 import styled from "styled-components";
 import { ThemeContainer } from "../../themes/common";
 import ModalTitleBar from "../../components/ModalViewer/ModalTitleBar";
@@ -13,33 +13,46 @@ export interface ModalBaseProps {
 const ModalBaseDefaultProps: ModalBaseProps = {};
 
 const ModalBase: React.FC<ModalBaseProps> = props => {
-   const { children } = { ...ModalBaseDefaultProps, ...props };
+   const { children } = {
+      ...ModalBaseDefaultProps,
+      ...props
+   };
    const { randomColor, randomDarkenedColor } = useRandomColor();
 
    const StyledModalBase = useMemo(
       () => styled.div`
+         position: relative;
          background: #fff;
          height: 75vh;
-         width: 95vw;
-         max-width: 95vw;
+         /* min-width: 40vw;
+         max-width: 95vw; */
          overflow: auto;
+         overflow-x: hidden;
          border: 1px solid rgba(0, 0, 0, 0.9);
          z-index: ${({ theme }: ThemeContainer) => theme.VARIABLES.LAYERS.MODAL};
          padding-top: 40px; /* Title Bar */
+         padding: 40px 40px 0 40px;
 
          &::-webkit-scrollbar {
             width: 0;
          }
 
          @media (min-width: ${({ theme }: ThemeContainer) => theme.VARIABLES.BREAK_POINTS.MEDIUM}) {
-            width: 80vw;
-            height: 85vh;
-            max-width: 900px;
             box-shadow: ${props => `10px 10px 0${props.shadowColor}`};
          }
 
          @media (min-width: ${({ theme }: ThemeContainer) => theme.VARIABLES.BREAK_POINTS.LARGE}) {
             max-width: 980px;
+         }
+
+         .gradient-bottom {
+            position: absolute;
+            left: 1px;
+            right: 1px;
+            bottom: 1px;
+            background: linear-gradient(180deg, hsla(0, 0%, 100%, 0) 0, #fff);
+            min-height: 40px;
+            max-height: 40px;
          }
       `,
       []
@@ -53,6 +66,7 @@ const ModalBase: React.FC<ModalBaseProps> = props => {
          <StyledModalBase shadowColor={randomDarkenedColor}>
             <ModalTitleBar bgColor={randomColor} />
             {children}
+            <div className="gradient-bottom" />
          </StyledModalBase>
       </RandomColorContextProvider>
    );
